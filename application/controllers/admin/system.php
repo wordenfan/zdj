2d6fa387d1c0bf45a088f48691430a9f529b71b0
benchao <benchao@17shihui.com>
2015-10-16 14:47:15 +0800

    修复后台配置bug

--- application/controllers/admin/system.php
+++ application/controllers/admin/system.php
@@ -22,10 +22,50 @@ class System extends AdminBase
     }
     //刷新和搜索
     public function conf_other(){
-        $this->load->view('admin/system/other',$this->data);
+        if($_POST){
+            foreach($_POST as $k=>$v){
+                if($k == 'title'){
+                    $where['name'] = AREA.'SITE_TITLE';
+                }else if($k == 'summary'){
+                    $where['name'] = AREA.'SITE_DESCRIPTION';
+                }else if($k == 'keyword'){
+                    $where['name'] = AREA.'SITE_KEYWORD';
+                }else if($k == 'bussiness_hour'){
+                    $where['name'] = AREA.'SERVICE_TIME';
+                }else if($k == 'tel'){
+                    $where['name'] = AREA.'SERVICE_PHONE';
+                }else if($k == 'area'){
+                    $where['name'] = AREA.'SERVICE_AREA';
+                }
+                $data['value'] = $v;
+                $this->db->where($where)->update(config_item('dbprefix').'config',$data);
+            }
+            $retrieve = array('status'=>1, 'data'=>'', 'msg'=>'更改成功');
+            echo json_encode($retrieve);
+            exit();
+        }else{
+            $this->load->view('admin/system/other',$this->data);
+        }
 	}
     public function conf()
     {
-        $this->load->view('admin/system/conf',$this->data);
+        if($_POST){
+            foreach($_POST as $k=>$v){
+                if($k == 'announce'){
+                    $where['name'] = AREA.'ANNOUNCEMENT_MODE';
+                }else if($k == 'announcement'){
+                    $where['name'] = AREA.'ANNOUNCEMENT';
+                }else if($k == 'stop_receive'){
+                    $where['name'] = AREA.'SITE_CLOSE';
+                }
+                $data['value'] = $v;
+                $this->db->where($where)->update(config_item('dbprefix').'config',$data);
+            }
+            $retrieve = array('status'=>1, 'data'=>'', 'msg'=>'更改成功');
+            echo json_encode($retrieve);
+            exit();
+        }else{
+            $this->load->view('admin/system/conf',$this->data);
+        }
     }
 }
