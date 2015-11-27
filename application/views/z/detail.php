78fde3d0ff78d5aade88b57284e044f23071f539
benchao <benchao@17shihui.com>
2015-11-28 00:48:44 +0800

    sdfasdf

--- application/views/z/detail.php
+++ application/views/z/detail.php
@@ -59,10 +59,10 @@
 					<?php endforeach;?>
 						</font><br>
                     <?php if($data[0]['user_status'] == 1):?>
-                    订单管理：<font color="red"><a href="http://www.163.com">撤销</a></font>
+                    订单管理：<font color="red"><a href="http://www.163.com">╯︿╰ 撤销 ╯︿╰</a></font>
 					<?php else:?>
-                    订单管理：<font color="green"><a href="http://www.163.com">接单</a></font>
-					<?php endif;?>
+                    订单管理：<font color="green"><a href="http://www.163.com">╯０╰ 接单 ╯０╰</a></font>
+					<?php endif;?><br>
 					食品清单：<br>
 					<?php echo $data[0]['food_list'];?>
 					<font color="red">备注：<?php echo $data[0]['remark'];?></font>
